a7987451254940eb71895b5fabee7e684ee8ad42
Roberta Velykiene <roberta.velykiene@gmail.com>
2019-04-23 22:52:49 +0300

    improve table structure

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,12 @@
-insert into transactionData
-values('ANDRIAUS_SEB','15c83e74-ebb1-4bb0-a2b7-83da2d508004','2019-01-15', 'First transaction', 200.00);
+insert into accountTable
+values ('15c83e74-ebb1-4bb0-a2b7-83da2d508004', 'Andrius SEB'),
+       ('15c83e74-ebb1-4bb0-a2b7-83da2d508005', 'Roberta SEB');
 
-insert into transactionData
-values('ROBERTOS_SEB','15c83e74-ebb1-4bb0-a2b7-83da2d508005','2019-02-5', 'Second transaction', -150.05);
+insert into transactionTable
+values ('15c83e74-ebb1-4bb0-a2b7-83da2d508004', '15c83e74-ebb1-4bb0-a2b7-83da2d508008', '2019-01-15',
+        'First transaction', 200.00),
+       ('15c83e74-ebb1-4bb0-a2b7-83da2d508005', '15c83e74-ebb1-4bb0-a2b7-83da2d508009', '2019-02-5',
+        'Second transaction', -150.05);
+
+
 
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,17 @@
-create table transactionData
+create table accountTable
 (
- accountId varchar(50) not null,
- id UUID not null,
- date date not null,
- description varchar(50) not null,
- amount decimal not null
+  id   varchar(50) not null,
+  name varchar(50) not null,
+  primary key (id),
+);
+
+create table transactionTable
+(
+  accountId   varchar(50) not null,
+  id          varchar(50) not null,
+  date        date        not null,
+  description varchar(50) not null,
+  amount      decimal     not null,
+  primary key (id),
+  foreign key (accountId) references accountTable (id)
 );
