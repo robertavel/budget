faf48b856875f6d402d527985f05772dfbaf084b
Roberta Velykiene <roberta.velykiene@gmail.com>
2019-05-02 13:19:27 +0300

    add all accounts to repo

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,8 @@
 insert into accountTable
-values ('15c83e74-ebb1-4bb0-a2b7-83da2d508004', 'Andrius SEB'),
-       ('15c83e74-ebb1-4bb0-a2b7-83da2d508005', 'Roberta SEB');
+values ('15c83e74-ebb1-4bb0-a2b7-83da2d508002', 'Andriaus piniginė'),
+       ('15c83e74-ebb1-4bb0-a2b7-83da2d508003', 'Robertos piniginė'),
+       ('15c83e74-ebb1-4bb0-a2b7-83da2d508004', 'Andriaus SEB'),
+       ('15c83e74-ebb1-4bb0-a2b7-83da2d508005', 'Robertos SEB');
 
 insert into transactionTable
 values ('15c83e74-ebb1-4bb0-a2b7-83da2d508004', '15c83e74-ebb1-4bb0-a2b7-83da2d508008', '2019-01-15',
